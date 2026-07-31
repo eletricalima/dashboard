0024bd188ff3b92b1c172c89b1a8276e096f9d5e
eletricalima <adson.lima@gmail.com>
2026-07-31 01:06:53 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-window.DASHBOARD_CONFIG = {
-  appsScriptUrl: "COLE_AQUI_A_URL_DO_APPS_SCRIPT",
-  atualizarAoAbrir: true,
-  intervaloAtualizacaoMinutos: 30,
-  limiteDadosDesatualizadosHoras: 24,
-};
