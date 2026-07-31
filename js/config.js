c9f422f87109945a291a13050dea84237c485c65
adson182 <adson.lima@gmail.com>
2026-07-31 00:34:33 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.DASHBOARD_CONFIG = {
+  appsScriptUrl: "COLE_AQUI_A_URL_DO_APPS_SCRIPT",
+  atualizarAoAbrir: true,
+  intervaloAtualizacaoMinutos: 30,
+  limiteDadosDesatualizadosHoras: 24,
+};
